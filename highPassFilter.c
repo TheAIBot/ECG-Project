f58166b6cf13f4af8448c420d036ffbb629b3407
TheAIBot <andreasuranus@hotmail.com>
2016-09-12 00:00:21 +0200

    replaced manuel array shift with memcpy call

--- highPassFilter.c
+++ highPassFilter.c
@@ -8,15 +8,11 @@ static int x_17 = 0;
 
 short highPassFilter(int x, int x_16, int x_32)
 {
-
 	short newY = rawHigh[Y_1_INDEX] - (x / 32) + x_16 - x_17 + (x_32 / 32);
 
 	x_17 = x_16;
 
-	rawHigh[4] = rawHigh[3];
-	rawHigh[3] = rawHigh[2];
-	rawHigh[2] = rawHigh[1];
-	rawHigh[1] = rawHigh[0];
+	memcpy( rawHigh + 1, rawHigh, sizeof(rawHigh));
 	rawHigh[0] = newY;
 
 	return newY;
