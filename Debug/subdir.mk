37231a263bbae4142d70efd4fb746d9907aedb07
TheAIBot <andreasuranus@hotmail.com>
2016-09-13 03:09:09 +0200

    made benchmarks better by taking the average of 20 runs(average is not the best way but it's the easiest way)

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -53,11 +53,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-<<<<<<< HEAD
-	gcc -O0 -g3 -pg -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-=======
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
->>>>>>> 48a47abfecfa058fba4bf62e6248e0a498aad29b
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
