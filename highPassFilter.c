be6b27a0cfe25225153f471ce8327e70e5b4d2f1
TheAIBot <andreasuranus@hotmail.com>
2016-09-23 15:18:24 +0200

    jesper fix the stuff

--- highPassFilter.c
+++ highPassFilter.c
@@ -10,7 +10,12 @@ static int x_17 = 0;
 
 short highPassFilter(const int x, const int x_16, const int x_32)
 {
+	//Following optimization is used to use less divides
+	//-(x / 32) + (x_32 / 32) = ((x_32 - x) / 32)
+	//short newY = GET_HIGH_PASS_VALUE(Y_1_INDEX) + ((x_32 - x) / 32) + x_16 - x_17;
 	short newY = GET_HIGH_PASS_VALUE(Y_1_INDEX) - (x / 32) + x_16 - x_17 + (x_32 / 32);
+	//printf("E: %d\n", - (x / 32) + (x_32 / 32));
+	//printf("G: %d\n", ((x_32 - x) / 32));
 	x_17 = x_16;
 	memcpy( rawHigh + 1, rawHigh, sizeof(rawHigh));
 	rawHigh[0] = newY;
